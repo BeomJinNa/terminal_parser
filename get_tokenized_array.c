e9e3646339c7b57c6764c2997a20f023a0859b65
Beomjin Na <bena@c10r8s1.42seoul.kr>
2023-06-29 20:33:58 +0900

    first commit

---
+++ get_tokenized_array.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_tokenized_array.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 16:41:12 by bena              #+#    #+#             */
+/*   Updated: 2023/06/29 20:32:02 by bena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int			get_number_of_tokens(const char *str);
+char		*get_next_token(const char **str);
+int			push_to_the_next_token(const char **str);
+static char	**remove_matrix(char **array);
+
+char	**get_tokenized_array(const char *str)
+{
+	int		size;
+	char	**output;
+	int		i;
+
+	size = get_number_of_tokens(str);
+	if (size == 0)
+		return (NULL);
+	output = (char **)malloc(sizeof(char *) * (size + 1));
+	if (output == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		output[i] = get_next_token(&str);
+		if (output[i] == NULL)
+			return (remove_matrix(output));
+		i++;
+	}
+	output[size] = NULL;
+	return (output);
+}
+
+int	get_number_of_tokens(const char *str)
+{
+	int	output;
+	int	token_size;
+
+	output = 0;
+	while (*str)
+	{
+		token_size = push_to_the_next_token(&str);
+		if (token_size > 0)
+			output++;
+		str += token_size;
+	}
+	return (output);
+}
+
+char	*get_next_token(const char **str)
+{
+	char	*output;
+	int		token_size;
+	int		i;
+
+	if (str == NULL || *str == NULL)
+		return (NULL);
+	token_size = push_to_the_next_token(str);
+	if (token_size == 0)
+		return (NULL);
+	output = (char *)malloc(sizeof(char) * (token_size + 1));
+	if (output == NULL)
+		return (NULL);
+	i = -1;
+	while (++i < token_size)
+		output[i] = (*str)[i];
+	output[token_size] = '\0';
+	*str += token_size;
+	return (output);
+}
+
+int	push_to_the_next_token(const char **str)
+{
+	const char	*ptr;
+	int			in_brace;
+	int			in_double_brace;
+
+	if (str == NULL || *str == NULL)
+		return (0);
+	in_brace = 0;
+	in_double_brace = 0;
+	while (**str && **str == ' ')
+		(*str)++;
+	ptr = *str;
+	while (*ptr)
+	{
+		if (*ptr == '\'' && in_double_brace == 0)
+			in_brace ^= 1;
+		else if (*ptr == '\"' && in_brace == 0)
+			in_double_brace ^= 1;
+		if (in_brace == 0 && in_double_brace == 0 && *ptr == ' ')
+			break ;
+		ptr++;
+	}
+	return (ptr - *str);
+}
+
+static char	**remove_matrix(char **array)
+{
+	char	**ptr;
+
+	if (array == NULL)
+		return (NULL);
+	ptr = array;
+	while (*ptr != NULL)
+	{
+		free(*ptr);
+		ptr++;
+	}
+	free(array);
+	return (NULL);
+}
---
+++ tokenizer.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bena <bena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/29 20:32:35 by bena              #+#    #+#             */
+/*   Updated: 2023/06/29 20:32:56 by bena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TOKENIZER_H
+# define TOKENZIER_H
+
+char	**get_tokenized_array(const char *str)
+int		get_number_of_tokens(const char *str);
+char	*get_next_token(const char **str);
+int		push_to_the_next_token(const char **str);
+#endif
